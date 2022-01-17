d44c07ed7c273e7283c2fca53e80a47de63d5675
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-01-17 22:30:44 +0800

    🩹更新插件标题和描述

--- src/aoaostar_com/code_css/Install.php
+++ src/aoaostar_com/code_css/Install.php
@@ -10,10 +10,10 @@ class Install implements \plugin\Install
 
     public function Install(Plugin $model)
     {
-        $model->title = 'CSS 格式化/混淆/压缩';
+        $model->title = 'CSS 格式化/压缩';
         $model->class = plugin_current_class_get(__NAMESPACE__);
         $model->alias = base_space_name(__NAMESPACE__);
-        $model->desc = '对CSS代码进行格式化/混淆/压缩';
+        $model->desc = '对CSS代码进行格式化/压缩';
         $model->version = 'v1.0';
     }
 
--- src/aoaostar_com/code_html/Install.php
+++ src/aoaostar_com/code_html/Install.php
@@ -10,10 +10,10 @@ class Install implements \plugin\Install
 
     public function Install(Plugin $model)
     {
-        $model->title = 'Html 格式化/混淆/压缩';
+        $model->title = 'Html 格式化/加密/压缩';
         $model->class = plugin_current_class_get(__NAMESPACE__);
         $model->alias = base_space_name(__NAMESPACE__);
-        $model->desc = '对Html代码进行格式化/混淆/压缩';
+        $model->desc = '对Html代码进行格式化/加密/压缩';
         $model->version = 'v1.0';
     }
 
