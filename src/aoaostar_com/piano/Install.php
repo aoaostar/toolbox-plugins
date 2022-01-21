b5b6dfdf382d4d4e740bc59e853a75fdcb1194a7
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-01-21 18:12:41 +0800

    🚩新增插件piano

---
+++ src/aoaostar_com/piano/Install.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace plugin\aoaostar_com\piano;
+
+
+use app\model\Plugin;
+
+class Install implements \plugin\Install
+{
+
+    public function Install(Plugin $model)
+    {
+        $model->title = '在线钢琴';
+        $model->class = plugin_current_class_get(__NAMESPACE__);
+        $model->alias = base_space_name(__NAMESPACE__);
+        $model->desc = '在线钢琴演奏，让只会敲代码你瞬间变成钢琴小王子';
+    }
+
+    public function UnInstall(Plugin $model)
+    {
+
+    }
+}
