05f4676e791b02014409d7b5705574a111eab0e8
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-04-16 18:35:07 +0800

    ✨ 新增GitHub图床
    💩 更新需要改进的糟糕代码

---
+++ src/aoaostar_com/github_image_hosting/App.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace plugin\aoaostar_com\github_image_hosting;
+
+use plugin\Drive;
+
+class App implements Drive
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function Index()
+    {
+        return msg("ok", "success", plugin_info_get());
+    }
+}
---
+++ src/aoaostar_com/github_image_hosting/Install.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace plugin\aoaostar_com\github_image_hosting;
+
+
+use app\model\Plugin;
+
+class Install implements \plugin\Install
+{
+
+    public function Install(Plugin $model)
+    {
+        $model->title = 'GitHub图床';
+        $model->class = plugin_current_class_get(__NAMESPACE__);
+        $model->alias = base_space_name(__NAMESPACE__);
+        $model->desc = '上传图片到GitHub并获取外链地址';
+        $model->version = 'v1.0';
+    }
+
+    public function UnInstall(Plugin $model)
+    {
+
+    }
+}
