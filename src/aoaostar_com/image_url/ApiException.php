2f76b427753b6fabbc35f7bfd8f31372749a98f6
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-08-08 21:42:06 +0800

    fix: 🐛 temporary file not deleted after image upload

--- src/aoaostar_com/image_url/ApiException.php
+++ src/aoaostar_com/image_url/ApiException.php
@@ -21,13 +21,9 @@ class ApiException extends Exception
         $this->data = $data;
     }
 
-    public function __construct($message = "", $data = [],$code = 0, Throwable $previous = null)
+    public function __construct($message = "", $data = [], $code = 0, Throwable $previous = null)
     {
         $this->setData($data);
-        $location = $this->getFile() . ':' . $this->getLine();
-        if (!empty($this->getData())){
-//            hidove_log($this->getData(), $location);
-        }
         parent::__construct($message, $code, $previous);
     }
 
--- src/aoaostar_com/image_url/App.php
+++ src/aoaostar_com/image_url/App.php
@@ -9,14 +9,14 @@ use think\facade\Validate;
 class App implements Drive
 {
 
+    private $path = "";
+
     public function __construct()
     {
-
-    }
-
-    public function Index()
-    {
-        return msg("ok", "success", plugin_info_get());
+        $this->path = plugin_path_get(plugin_current_class_get(__NAMESPACE__));
+        require "$this->path/Plugin.php";
+        require "$this->path/common.php";
+        require "$this->path/ApiException.php";
     }
 
     public function upload()
@@ -36,37 +36,38 @@ class App implements Drive
             return msg('error', $validate->getError());
         }
 
-        $filename = uniqid() . '.' . $uploadedFile->extension();
-        $tmpDir = app()->getRuntimePath() . '/tmp';
-        if (!is_dir($tmpDir)) {
-            mkdir($tmpDir, 0777, true);
-        }
-        $uploadedFile->move($tmpDir, $filename);
-
-        $path = plugin_path_get(plugin_current_class_get(__NAMESPACE__));
-
-        require "$path/Plugin.php";
-        require "$path/common.php";
-        require "$path/ApiException.php";
-        if (!file_exists("$path/nodes/$node/main.php")) {
+        if (!file_exists("$this->path/nodes/$node/main.php")) {
             return msg('error', "该节点[$node]不存在");
         }
-        require "$path/nodes/$node/main.php";
+        require "$this->path/nodes/$node/main.php";
 
         if (!class_exists($node)) {
             return msg('error', "该节点[$node]不存在");
         }
-        $instance = new $node();
-        try {
-            $res = $instance->main($tmpDir . DIRECTORY_SEPARATOR . $filename);
-        } catch (\Exception $e) {
-
-            return msg('error', $e->getMessage());
+        $filename = uniqid() . '.' . $uploadedFile->extension();
+        $tmpDir = app()->getRuntimePath() . '/tmp';
+        if (!is_dir($tmpDir)) {
+            mkdir($tmpDir, 0644, true);
         }
+        try {
 
+            $uploadedFile->move($tmpDir, $filename);
+            $instance = new $node();
+            $res = $instance->main($tmpDir . DIRECTORY_SEPARATOR . $filename);
 
-        return msg('ok', "success", [
-            'url' => $res,
-        ]);
+            return msg('ok', "success", [
+                'url' => $res,
+            ]);
+
+        } catch (\Exception $e) {
+            return msg('error', $e->getMessage());
+        } finally {
+            @unlink($tmpDir . DIRECTORY_SEPARATOR . $filename);
+        }
+    }
+
+    public function Index()
+    {
+        // TODO: Implement Index() method.
     }
 }
