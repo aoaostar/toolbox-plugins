b00b9dd310ebe877a3ab87d8bcc2ceaad9ea95f1
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-09-20 13:26:10 +0800

    fix: 🐛 修复随机图片接口失效

--- src/aoaostar_com/random_beauty_images/Install.php
+++ src/aoaostar_com/random_beauty_images/Install.php
@@ -14,7 +14,7 @@ class Install implements \plugin\Install
         $model->class = plugin_current_class_get(__NAMESPACE__);
         $model->alias = base_space_name(__NAMESPACE__);
         $model->desc = '随机返回一张美图';
-        $model->version = 'v1.1';
+        $model->version = 'v1.2';
     }
 
     public function UnInstall(Plugin $model)
