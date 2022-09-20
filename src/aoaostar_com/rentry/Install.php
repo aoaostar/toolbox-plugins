4053ad6976c3f0c2b3cc65199f7f2be6a8f5aead
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-09-20 12:50:21 +0800

    feat: 🎸 新增Rentry 文字分享

---
+++ src/aoaostar_com/rentry/Install.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace plugin\aoaostar_com\rentry;
+
+
+use app\model\Plugin;
+
+class Install implements \plugin\Install
+{
+    # 安装时运行方法
+    public function Install(Plugin $model)
+    {
+        # 标题
+        $model->title = "Rentry 文字分享";
+        # 类名、无需修改
+        $model->class = plugin_current_class_get(__NAMESPACE__);
+        # 路由、即 example
+        $model->alias = base_space_name(__NAMESPACE__);
+        # 描述
+        $model->desc = '分享你的文字、代码';
+        $model->template = 'redirect';
+        $model->config = [
+            'url' => 'https://rentry.co/'
+        ];
+        # 版本号
+        $model->version = 'v1.0';
+    }
+
+    # 卸载时运行方法
+    public function UnInstall(Plugin $model)
+    {
+
+    }
+}
