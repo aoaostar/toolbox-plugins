f319195f846624a1fcbaca5f9daa84da40669fa0
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-10-19 17:48:13 +0800

    feat: 🎸 新增文本去重

---
+++ src/aoaostar_com/text_deduplicate/App.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace plugin\aoaostar_com\text_deduplicate;
+
+use plugin\Drive;
+
+class App implements Drive
+{
+    public function Index()
+    {
+        // TODO: Implement Index() method.
+    }
+}
---
+++ src/aoaostar_com/text_deduplicate/Install.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace plugin\aoaostar_com\text_deduplicate;
+
+
+use app\model\Plugin;
+
+class Install implements \plugin\Install
+{
+    # 安装时运行方法
+    public function Install(Plugin $model)
+    {
+        # 标题
+        $model->title = '文本去重';
+        # 类名、无需修改
+        $model->class = plugin_current_class_get(__NAMESPACE__);
+        # 路由、即 example
+        $model->alias = base_space_name(__NAMESPACE__);
+        # 描述
+        $model->desc = '将列表内容放入文本框内，每行一条，点击去重按钮，即可得到无重复数据的列表）';
+        # 版本号
+        $model->version = 'v1.0';
+    }
+
+    # 卸载时运行方法
+    public function UnInstall(Plugin $model)
+    {
+
+    }
+}
