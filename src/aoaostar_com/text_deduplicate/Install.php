3fbe44b4f2598bb0761e9c55f584d0d4d1287db2
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-10-19 18:00:29 +0800

    docs: ✏️ 修正`文本去重`描述

--- src/aoaostar_com/text_deduplicate/Install.php
+++ src/aoaostar_com/text_deduplicate/Install.php
@@ -17,7 +17,7 @@ class Install implements \plugin\Install
         # 路由、即 example
         $model->alias = base_space_name(__NAMESPACE__);
         # 描述
-        $model->desc = '将列表内容放入文本框内，每行一条，点击去重按钮，即可得到无重复数据的列表）';
+        $model->desc = '将列表内容放入文本框内，每行一条，点击去重按钮，即可得到无重复数据的列表';
         # 版本号
         $model->version = 'v1.0';
     }
